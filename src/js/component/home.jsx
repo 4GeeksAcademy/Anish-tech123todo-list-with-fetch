ebb0d23234952870efe38df63d74a05f72d699aa
Anish-tech123 <rvermaji321@alumni.iastate.edu>
2024-09-14 21:22:39 +0000

    added new features

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,42 +1,129 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 //create your first component
 const Home = () => {
-	const [toDoList, setToDoList] = useState([
-		{
-			label: "take out Trash",
-			done: false
-		},
-		{
-			label: "feed the dog",
-			done: false
-		}
-	])
+	const [toDoList, setToDoList] = useState([]);
 	const [inputValue, setInputValue] = useState("");
 	const [hovered, setHovered] = useState(null);
 
+	useEffect(() => {
+		fetchTodos(setToDoList);
+	}, [])
+
+	const API_URL = "https://playground.4geeks.com/todo";
+	const user = "Rishi"
+
+	const fetchTodos = (setToDoList) => {
+		fetch(`${API_URL}/users/${user}`)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("fail to fetch users" + response.status)
+				}
+				console.log("reposnse from API: ", response)
+				return response.json()
+			})
+			.then((data) => {
+				console.log("toDoList from API", data)
+				setToDoList(data && Array.isArray(data.todos) ? data.todos : []);
+			})
+			.catch((error) => {
+				console.error("Fetched to do fail", error)
+			})
+
+	}
+
 	const addToDo = (e) => {
 		if (e.key == "Enter" && inputValue != "") {
-			let newList = toDoList.concat({
-				label: inputValue,
-				done: false
+			fetch("https://playground.4geeks.com/todo/todos/Rishi", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					label: inputValue,
+					is_done: false
+				})
 			})
-			setToDoList(newList)
+				.then((response) => {
+					if (!response.ok) {
+						alert("click the add user button to add tasks");
+
+					}
+					console.log("new to do added:", inputValue);
+					fetchTodos(setToDoList)
+				})
+				.catch((error) => {
+					console.error("add task failed :", error);
+
+				})
 			setInputValue("")
 		}
 	};
 
 	const deleteToDo = (index) => {
-		let newList = toDoList.filter((item, idx) => idx != index)
-		setToDoList(newList)
+		let todoId = toDoList[index].id;
+		let deletedTodo = toDoList[index].label;
+
+		fetch(`${API_URL}/todos/${todoId}`, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then((response) => {
+				if (response.ok) {
+					console.log("to do deleted:", deletedTodo);
+					fetchTodos(setToDoList)
+				}
+			})
+			.catch((error) => {
+				console.error("delete task failed :", error);
+			})
+	}
+	const handleDeleteUser = () => {
+		fetch(`${API_URL}/users/Rishi`, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then((response) => {
+				if (response.ok) {
+					console.log("user and tos deleted");
+					setToDoList([])
+					alert("your tasks are gone")
+				}
+			})
+			.catch((error) => {
+				console.error("delete user failed :", error);
+			})
+	};
+
+	const handleAddUser = () => {
+		fetch(`${API_URL}/users/Rishi`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then((response) => {
+				if (response.ok) {
+					console.log("user andded to API");
+					fetchTodos(setToDoList);
+					alert("you can now save tasks");
+				}
+			})
+			.catch((error) => {
+				console.error("adding user failed :", error);
+			})
 	}
 
 	return (
 		<div className="text-center">
 			<h1>todos</h1>
 			<div>
-				<input value={inputValue} onChange={e => setInputValue(e.target.value)} onKeyDown={e => addToDo(e)} />
+				<input value={inputValue} onChange={e => setInputValue(e.target.value)} onKeyDown={addToDo} />
 			</div>
 			<ul className="list-group w-50 mx-auto mt-5">
 				{toDoList.map((item, index) => {
@@ -49,6 +136,11 @@ const Home = () => {
 				})}
 				<li className="list-group-item">{toDoList.length == 0 ? "No tasks, add a task" : toDoList.length == 1 ? "1 item left" : toDoList.length + " items left"}</li>
 			</ul>
+			<div className="d-flex justify-content-center mt-4">
+				<button onClick={handleDeleteUser} className="btn btn-danger">delete all tasks and user</button>
+
+				<button onClick={handleAddUser} className="btn btn-success ms-3">add user to save tasks</button>
+			</div>
 		</div>
 	);
 };
--- src/js/updateApi.js
+++ src/js/updateApi.js
@@ -1,6 +1,34 @@
 const API_URL="https://playground.4geeks.com/todo";
 const user ="Rishi"
 
-export const fetchTodo = ()=>{
+export const fetchTodo = (setToDoList)=>{
+    fetch(`${API_URL}/users/${user}`)
+        .then((response)=> {
+            if (!response.ok) {
+                throw new Error("fail to fetch users"+response.status)
+            }
+            console.log("reposnse from API: ", response)
+            return response.json()
+        })
+        .then((data)=> {
+            console.log("toDoList from API", data)
+            setToDoList(data && Array.isArray(data.todos) ? data.todos: []);
+        })
+        .catch((error)=> {
+            console.error("Fetched to do fail" , error)
+        })
 
 }
+
+// .then((data) => {
+//     console.log("Todo List from API", data);
+//     if (Array.isArray(data.todos)) {
+//         setTodos(data.todos);
+//     } else {
+//         console.error("Fetched data is not an array:", data.todos);
+//         setTodos([]);
+//     }
+// })
+// .catch((error) => {
+//     console.error("There has been a problem with your fetch operation:", error);
+// });
